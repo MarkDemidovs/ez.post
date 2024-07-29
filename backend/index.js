2fe7679139ca857a42aba8e4a701f3c2249badcc
MarkDemidovs <mark@mark.net>
2024-07-29 13:58:43 +0300

    Basic Backend BoilerPlate

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+import express from "express"
+import {PORT} from "./config.js"
+
+const app = express()
+
+app.get("/", (req,res) => {
+    console.log(req)
+    return res.status(200).json({"message": "welcome"})
+});
+
+app.listen(PORT, () => {
+    console.log("listening to port 5000");
+})

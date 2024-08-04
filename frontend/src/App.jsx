c5dce40fce72d60a1229aa3d35bd52520f11ed84
MarkDemidovs <markdemidovs@proton.me>
2024-08-04 11:59:28 +0300

    Author titles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -32,8 +32,9 @@ export default function App() {
       <h1>Post Titles</h1>
       <ul>
         {posts.map((post) => (
-          <li key={post._id}>{post.title}</li> // Assuming each post has a unique '_id' field
+          <li key={post._id}>{post.title} <span className="authorTitle">Created by </span> <span className="authorName">{post.author}</span></li> // Assuming each post has a unique '_id' field
         ))}
+        
       </ul>
     </div>
   );

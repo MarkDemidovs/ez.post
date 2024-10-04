6ee02b7a970485609da037f1e0d96f341d5df81d
MarkDemidovs <markdemidovs@proton.me>
2024-10-04 16:57:04 +0300

    web header update

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -3,7 +3,7 @@ export default function NavBar() {
             <div id="topBar">
                 <h4>ez.posts</h4>
 
-                <h5>defining anonymity.</h5>
+                <h5>defining anonymity. (v1.0)</h5>
             </div>
     )
 }

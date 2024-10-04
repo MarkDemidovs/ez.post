366052b5bb8a7dcce78b1b45dff15a096370af63
MarkDemidovs <markdemidovs@proton.me>
2024-10-04 17:28:00 +0300

    github link

--- frontend/src/footer.jsx
+++ frontend/src/footer.jsx
@@ -2,6 +2,7 @@ export default function Footer() {
     return(
         <div id="footer">
         <p>Made by Mark | Have any questions / reports? markdemidovs@proton.me</p>
+        <a href="https://github.com/MarkDemidovs/ez.post">github</a>
         </div>
     )
 }
